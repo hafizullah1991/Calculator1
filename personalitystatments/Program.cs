df15b3a4c4aa6af81f63acc991d52baf28506605
Hafiz Hamdard <ahmadshakhi2020@gmail.com>
2024-01-15 20:18:12 -0800

    calculator

---
+++ personalitystatments/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace personalitystatments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+            double result = 0;
+
+            Console.WriteLine("enter number1: ");
+            double x= double.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter number2 : ");
+            double y= double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("select select one:");
+            Console.WriteLine("\t +  -  *   /   ");
+
+            switch(Console.ReadLine())
+            {
+                case "+":
+                    result = x + y;
+                    Console.WriteLine($"{x} + {y} is {result}");
+                    break;
+                    case "-": result = x - y;
+                    Console.WriteLine($"{x} {y} is your {result}");
+                    break;
+                    case "*": result = x * y;
+                    Console.WriteLine($"{x}*{y} is your {result}");
+                    break;
+
+                    case "/": result = x / y;
+                    Console.WriteLine($"{x} / {y} is your {result}");
+                    break;
+            }
+        }
+    }
+}
+
+            
---
+++ personalitystatments/personality.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    internal class personality
+    {
+
+        Console.WriteLine("Welcome to the Chuckle Code Zone!");
+
+        Console.WriteLine("Are you ready for a laugh? Let's test your sense of humor.");
+
+        Console.WriteLine("Enter 'yes' if you're ready, 'no' if you need a moment:");
+    }
+}
